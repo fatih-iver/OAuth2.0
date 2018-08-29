7c29e136725e00200e7625e7f2a469f5a9100a0c
vagrant <vagrant@vagrant.vm>
2018-08-29 12:24:49 +0000

    Fix Thread Error

--- database_setup.py
+++ database_setup.py
@@ -2,12 +2,12 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
- 
+
 Base = declarative_base()
 
 class Restaurant(Base):
     __tablename__ = 'restaurant'
-   
+
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
@@ -18,7 +18,7 @@ class Restaurant(Base):
            'name'         : self.name,
            'id'           : self.id,
        }
- 
+
 class MenuItem(Base):
     __tablename__ = 'menu_item'
 
@@ -45,7 +45,7 @@ class MenuItem(Base):
 
 
 
-engine = create_engine('sqlite:///restaurantmenu.db')
- 
+engine = create_engine('sqlite:///restaurantmenu.db', connect_args={'check_same_thread': False})
+
 
 Base.metadata.create_all(engine)
